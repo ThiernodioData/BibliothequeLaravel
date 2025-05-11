013a0d3bb1da56d3d3baaada49a84b67e9eb90da
udev <diallothiernoidrissa352@gmail.com>
2025-05-11 16:06:31 +0000

    send end

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@ Route::get('/', [BookController::class, 'home'])->name('home');
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
-Route::post('/books/{id}', [BookController::class, 'store'])->name('books.store');
-Route::post('/reviews', [ReviewController::class, 'store']);
 Route::post('/books/{book}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
 
 
